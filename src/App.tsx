0260e79582785af372e286aca9d99865ac8fd6f1
Suyog Poudel <suyogpoudel583@gmail.com>
2025-06-30 17:52:34 +0545

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import {IconClock} from "@tabler/icons-react";
-
 const App = () => {
     return (
         <div className='flex justify-center items-center h-screen'>
-            <div>
+            <div className='flex flex-col items-center text-center'>
                 <h2 className='text-5xl text-slate-900'>This website is currently in development :)</h2>
-                <h3 className='text-3xl text-slate-900 opacity-75'>Will be active soon....<IconClock/></h3>
+                <h3 className='text-3xl text-slate-900 opacity-75 flex items-center justify-center'>Will be active
+                    soon....
+                </h3>
             </div>
         </div>
     )
