f1b581e2ee30c8cd47dd23cf6f502e8e88161fdb
Suyog Poudel <80449144+suyogpoudel@users.noreply.github.com>
2025-07-01 22:58:33 +0545

    added responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ const App = () => {
     return (
         <div className='flex justify-center items-center h-screen'>
             <div className='flex flex-col items-center text-center'>
-                <h2 className='text-5xl text-slate-900'>This website is currently in development :)</h2>
-                <h3 className='text-3xl text-slate-900 opacity-75 flex items-center justify-center'>Will be active
+                <h2 className='text-5xl text-slate-900 max-sm:text-3xl'>This website is currently in development :)</h2>
+                <h3 className='text-3xl text-slate-900 opacity-75 flex items-center justify-center max-sm:text-xl'>Will be active
                     soon....
                 </h3>
             </div>
