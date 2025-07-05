9268c867f1582d0ee3ee96f8eb0f0fe7f4724263
Suyog Poudel <suyogpoudel583@gmail.com>
2025-07-05 16:37:20 +0545

    temp

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,11 @@
 import {
-    IconBrandCss3,
-    IconBrandHtml5,
-    IconBrandJavascript,
-    IconBrandReact, IconBrandTailwind,
-    IconBrandTypescript, IconCalendar, IconCompass
+    // IconBrandCss3,
+    // IconBrandHtml5,
+    // IconBrandJavascript,
+    // IconBrandReact, IconBrandTailwind,
+    // IconBrandTypescript,
+    IconCalendar, IconCompass
 } from "@tabler/icons-react";
-import Skill from "./Skill";
 
 
 const About = () => {
@@ -32,12 +32,12 @@ const About = () => {
 
                 <p className='text-[24px] opacity-90 mt-5 text-center'>Skills:</p>
                 <ul className='grid grid-cols-2 gap-5 max-sm:gap-2 place-items-center mt-2'>
-                    <Skill title='HTML' icon={<IconBrandHtml5/>}/>
-                    <Skill title='CSS' icon={<IconBrandCss3/>}/>
-                    <Skill title='Tailwind' icon={<IconBrandTailwind/>}/>
-                    <Skill title='Javascript' icon={<IconBrandJavascript/>}/>
-                    <Skill title='Typescript' icon={<IconBrandTypescript/>}/>
-                    <Skill title='React' icon={<IconBrandReact/>}/>
+                    {/*<Skill title='HTML' icon={<IconBrandHtml5/>}/>*/}
+                    {/*<Skill title='CSS' icon={<IconBrandCss3/>}/>*/}
+                    {/*<Skill title='Tailwind' icon={<IconBrandTailwind/>}/>*/}
+                    {/*<Skill title='Javascript' icon={<IconBrandJavascript/>}/>*/}
+                    {/*<Skill title='Typescript' icon={<IconBrandTypescript/>}/>*/}
+                    {/*<Skill title='React' icon={<IconBrandReact/>}/>*/}
                 </ul>
             </div>
 
